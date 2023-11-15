a29342baf4861d3a0a78aff5d5274af7e57b2ce4
Baiqiang <qiyuuu@gmail.com>
2023-11-15 12:25:48 +0800

    fix: optimize copy button

--- components/sequence.vue
+++ components/sequence.vue
@@ -12,16 +12,16 @@ const { copy, copied } = useClipboard()
   <div class="flex items-start gap-1">
     <pre v-if="!html" class="sequence"><span v-if="prefix" class="select-none">{{ prefix }}</span>{{ sequence }}</pre>
     <pre v-else class="sequence" v-html="sequence" />
-    <div class="w-20 flex flex-col md:flex-row items-center justify-center md:items-start md:justify-start pt-1">
+    <div v-if="source" class="w-10 flex flex-col md:flex-row items-center justify-center md:items-start md:justify-start pt-1">
+      <div v-if="copied" class="text-xs text-gray-500">
+        Copied
+      </div>
       <Icon
-        v-if="source"
+        v-else
         name="ion:ios-copy"
         class="cursor-pointer text-indigo-500"
         @click="copy(source)"
       />
-      <div v-if="copied" class="text-xs text-gray-500">
-        Copied
-      </div>
     </div>
   </div>
 </template>
