4a8c015e405f66c2db2f3bbcc7d84bcdc2f476ac
Baiqiang <qiyuuu@gmail.com>
2023-12-14 17:51:57 +0800

    feat: add button to check origin solution

--- components/submission.vue
+++ components/submission.vue
@@ -3,7 +3,11 @@ const props = defineProps<{
   submission: Submission
 }>()
 const showComment = ref(false)
+const showOriginal = ref(false)
 const solution = computed(() => {
+  if (showOriginal.value)
+    return props.submission.solution
+
   try {
     return formatAlgorithm(props.submission.solution)
   }
@@ -17,6 +21,12 @@ const solution = computed(() => {
   <div>
     <div class="flex gap-2 justify-start items-start">
       <Sequence :sequence="solution" />
+      <button class="text-indigo-500" @click="showOriginal = !showOriginal">
+        <Icon
+          :name="showOriginal ? 'ic:sharp-rotate-90-degrees-cw' : 'ic:sharp-rotate-90-degrees-ccw'"
+          size="20"
+        />
+      </button>
       <button v-if="submission.comment.trim() !== ''" class="text-indigo-500" @click="showComment = !showComment">
         <Icon
           :name="showComment ? 'solar:alt-arrow-up-bold' : 'solar:alt-arrow-down-bold'"
