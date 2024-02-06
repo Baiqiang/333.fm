5db3ec7bd1734d90e7ebbd08884ac8f37de4ac7a
Baiqiang <qiyuuu@gmail.com>
2024-02-06 22:35:44 +0800

    feat: add filter for user submissions

--- components/pager.vue
+++ components/pager.vue
@@ -1,21 +1,26 @@
 <script setup lang="ts">
-defineProps<{
+const props = defineProps<{
   page: string | number
   isCurrent?: boolean
 }>()
-const router = useRouter()
 const route = useRoute()
-function goPage(page: number | string) {
-  const query = { ...route.query }
-  if (page === 1)
-    delete query.page
-  else query.page = page.toString()
-  router.push({ ...route, query })
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' })
 }
+const link = computed(() => {
+  const page = props.page.toString() === '1' ? undefined : props.page.toString()
+  return {
+    ...route,
+    query: {
+      ...route.query,
+      page,
+    },
+  }
+})
 </script>
 
 <template>
-  <div class="px-3 py-1" :class="{ 'bg-blue-500 text-white cursor-pointer': !isCurrent, 'bg-gray-200 text-black': isCurrent }" @click="goPage(page)">
+  <NuxtLink :to="link" class="px-3 py-1" :class="{ 'bg-blue-500 text-white cursor-pointer': !isCurrent, 'bg-gray-200 text-black': isCurrent }" @click="scrollToTop">
     {{ page }}
-  </div>
+  </NuxtLink>
 </template>
--- pages/profile/[id].vue
+++ pages/profile/[id].vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
+import type { RouteLocationRaw } from '#vue-router'
+
 const route = useRoute()
+const { t } = useI18n()
 const { data, error } = await useApi<User>(`/profile/${route.params.id}`)
 if (error.value || !data.value) {
+  console.log(error.value)
   throw createError({
     statusCode: error.value?.statusCode ?? 404,
   })
@@ -15,23 +19,61 @@ const meta: Ref<PaginationMeta> = ref({
   totalPages: 0,
   currentPage: 1,
 })
+const filters = computed(() => {
+  if (meta.value.totalItems === 0)
+    return []
+  const filters: { to: RouteLocationRaw, label: string }[] = [{
+    to: {
+      ...route,
+      query: {
+        ...route.query,
+        type: undefined,
+        page: undefined,
+      },
+    },
+    label: `${t('common.all')} (${meta.value.totalItems})`,
+  }]
+  for (const filter of (meta.value as any).filters || []) {
+    let label = ''
+    switch (filter.type) {
+      case CompetitionType.WEEKLY:
+        label = t('weekly.title')
+        break
+      case CompetitionType.ENDLESS:
+        label = t('endless.title')
+        break
+    }
+    filters.push({
+      to: {
+        ...route,
+        query: {
+          ...route.query,
+          type: filter.type,
+          page: undefined,
+        },
+      },
+      label: `${label} (${filter.count})`,
+    })
+  }
+  return filters
+})
+await fetchData()
+watch(() => route.query, async () => {
+  await fetchData()
+}, {
+  deep: true,
+})
 async function fetchData() {
   const { data } = await useApi<Pagination<Submission>>(`/profile/${route.params.id}/submissions`, {
     params: {
       page: route.query.page,
+      type: route.query.type,
       limit: DEFAULT_LIMIT,
     },
   })
   submissions.value = data.value!.items
   meta.value = data.value!.meta
 }
-watch(() => route.query.page, async () => {
-  await fetchData()
-}, {
-  // immediate: true,
-  deep: true,
-})
-await fetchData()
 useSeoMeta({
   title: user.value.name,
 })
@@ -46,6 +88,11 @@ useSeoMeta({
       <a v-if="user.wcaId" :href="`https://www.worldcubeassociation.org/persons/${user.wcaId}`" target="_blank" class="text-blue-500">
         <img src="https://www.worldcubeassociation.org/files/WCAlogo_notext.svg" class="w-6">
       </a>
+    </div>
+    <div v-if="filters.length > 0" class="flex gap-2 mt-2">
+      <NuxtLink v-for="{ to, label }, i in filters" :key="i" :to="to" class="px-2 py-2 text-white bg-indigo-500">
+        {{ label }}
+      </NuxtLink>
     </div>
     <Submissions :submissions="submissions" spoiler />
     <Pagination :meta="meta" />
