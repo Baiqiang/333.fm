a531ab2b09f039f93fb6d6818cd4a55ddbc49026
Baiqiang <qiyuuu@gmail.com>
2024-10-21 09:47:08 +0800

    feat: add border

--- components/submission/attachments.vue
+++ components/submission/attachments.vue
@@ -5,7 +5,7 @@ defineProps<{
 </script>
 
 <template>
-  <div v-if="attachments && attachments.length > 0" v-viewer class="flex">
-    <img v-for="{ id, url, name } in attachments" :key="id" :src="url" :alt="name" class="w-8 h-8 cursor-pointer">
+  <div v-if="attachments && attachments.length > 0" v-viewer class="flex gap-1">
+    <img v-for="{ id, url, name } in attachments" :key="id" :src="url" :alt="name" class="w-12 h-12 border border-indigo-500 cursor-pointer">
   </div>
 </template>
