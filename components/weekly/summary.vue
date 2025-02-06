c06c2e00b48f4c4494d6902a6eb5f0d1265ee608
Baiqiang <qiyuuu@gmail.com>
2025-02-06 09:44:31 +0800

    feat: move join now btn up

--- components/weekly/summary.vue
+++ components/weekly/summary.vue
@@ -6,15 +6,15 @@ defineProps<{
 
 <template>
   <div>
+    <div class="float-right">
+      <NuxtLink :to="competitionPath(competition)" class="bg-indigo-500 text-white px-3 py-2 text-sm">
+        {{ $t('weekly.join') }}
+      </NuxtLink>
+    </div>
     <WeeklyStatus :competition="competition" />
     <div v-for="{ scramble, number } in competition.scrambles" :key="number" class="mt-2">
       <Sequence :sequence="scramble" :source="scramble" :prefix="`No.${number} `" />
       <CubeExpanded :moves="scramble" />
-    </div>
-    <div class="mt-4">
-      <NuxtLink :to="competitionPath(competition)" class="bg-indigo-500 text-white px-3 py-2 text-lg">
-        {{ $t('weekly.join') }}
-      </NuxtLink>
     </div>
   </div>
 </template>
